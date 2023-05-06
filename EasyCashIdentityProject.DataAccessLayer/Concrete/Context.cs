8aa1fb5900c8e6c392baa93cac0ed041bfdf3a2a
wixisoftware.com <122645167+wixisoftware@users.noreply.github.com>
2023-05-06 23:19:20 +0300

    Ders 5 Sonu

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+
+        //Özünde DbContext 'ten gelmektedir.
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=WIXI\\WIXI; initial catalog=EasyCashDb; integrated Security=true;");
+           
+        }
+
+        // burda verilen isim DbSet içerisnde olan cs tarafında tablonun adı sağında olan isim ise sql tarafında oluşturulucak olan tablonun ismidir.
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
